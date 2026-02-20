a518c5752e757204e1d14128f1007dfb44e6fab8
anuragkumar-git <patelanurag3971@gmail.com>
2026-02-20 22:12:43 +0530

    feat(pwa): configure service worker and manifest

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'Flux',
+        short_name: 'Flux',
+        description: 'Offline first Time Tracking PWA',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/pwa.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa1.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+
+    })
+  ],
 })
